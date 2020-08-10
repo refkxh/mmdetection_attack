b9685030140ce424d0acfb561bbbc502fdb24430
孔祥浩 <18373109@buaa.edu.cn>
2020-08-10 15:12:54 +0800

    try

--- attack.py
+++ attack.py
@@ -1,6 +1,7 @@
 from mmdet.apis import init_detector, inference_detector
 import mmcv
 import os
+import time
 
 config_file = 'configs/cascade_rcnn/cascade_rcnn_r101_fpn_1x_coco.py'
 checkpoint_file = 'checkpoints/cascade_rcnn_r101_fpn_1x_coco_20200317-0b6a2fbf.pth'
@@ -15,5 +16,10 @@ model = init_detector(config_file, checkpoint_file, device='cuda:0')
 names = os.listdir(input_dir)
 for cnt, name in enumerate(names):
     img = mmcv.imread(input_dir + name)
+    start_t = time.time()
     result = inference_detector(model, img)
-    model.show_result(img, result, out_file='outputs/{}'.format(name))
+    end_t = time.time()
+    cost_t = 1000 * (end_t - start_t)
+    print(result)
+    print("===>successfully processed img %d, cost %.2f ms." % (cnt + 1, cost_t))
+    # model.show_result(img, result, out_file=(output_dir + name))
